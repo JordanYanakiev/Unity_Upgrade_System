caa7762cee4f450d1bd519bd7ed18d26d4af603c
JordanYanakiev <jordan.yanakiev@gmail.com>
2025-01-12 12:29:42 +0200

    Initial commit

---
+++ Upgrades System/Assets/Upgrades System/Upgrade.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+    public enum IngameResources
+    {
+
+    }
+
+    public float bonusPercent;
+    public List<IngameResources> resourceRequirements;
+
+}
