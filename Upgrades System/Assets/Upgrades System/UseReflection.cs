f05f7aa79d3de487573c0712e0295ce5d78d7af9
JordanYanakiev <jordan.yanakiev@gmail.com>
2025-01-16 22:21:22 +0200

    Trying to use reflection to get all Lists of upgrades so I can create dynamic method for retrieving all List and storing them in saveandLoad class

---
+++ Upgrades System/Assets/Upgrades System/UseReflection.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class UseReflection : MonoBehaviour
+{
+    public UpgradesSO upgradesSO; // Assign your ScriptableObject in the Inspector
+
+    void Start()
+    {
+        // Get all lists of Upgrade and log upgrade names
+        List<IList> allLists = GetAllUpgradeLists(upgradesSO);
+        LogUpgradeNames(allLists);
+    }
+
+    List<IList> GetAllUpgradeLists(UpgradesSO scriptableObject)
+    {
+        List<IList> lists = new List<IList>();
+
+        // Use reflection to find all fields in the ScriptableObject
+        FieldInfo[] fields = scriptableObject.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var field in fields)
+        {
+            // Check if the field is a List<Upgrade>
+            if (field.FieldType.IsGenericType && field.FieldType.GetGenericTypeDefinition() == typeof(List<>))
+            {
+                // Ensure it's specifically a List<Upgrade>
+                if (field.FieldType.GetGenericArguments()[0] == typeof(Upgrade))
+                {
+                    // Get the value of the field and add it to the list
+                    object value = field.GetValue(scriptableObject);
+                    if (value is IList list)
+                    {
+                        lists.Add(list);
+                    }
+                }
+            }
+        }
+
+        return lists;
+    }
+
+    void LogUpgradeNames(List<IList> allLists)
+    {
+        foreach (var list in allLists)
+        {
+            foreach (var item in list)
+            {
+                if (item is Upgrade upgrade)
+                {
+                    Debug.Log($"Upgrade Name: {upgrade.upgradeName}");
+                }
+            }
+        }
+    }
+}
